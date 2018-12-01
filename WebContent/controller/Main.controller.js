b22f665fa2935166a678209fa7e429991a9c3352
kisbucedli <kisbucedli@gmail.com>
2018-12-01 15:18:19 +0100

    File loaded and set to the main view

--- WebContent/controller/Main.controller.js
+++ WebContent/controller/Main.controller.js
@@ -8,7 +8,12 @@ sap.ui.define([
 		return Controller.extend("com.work.attendance.controller.Main", {
 			
 			onInit : function () {
-				var oModel;
+				
+			    //var oModel = new sap.ui.model.json.JSONModel("file:///C:/Users/julia.gondocs/OneDrive%20-%20Accenture/SAPUI5/Project/Attendance/Attendance.json");
+			    
+	            var oModel= new sap.ui.model.json.JSONModel();
+	            oModel.loadData("./Attendance.json");
+			    this.getView().setModel(oModel,"attendances");
 			},
 			showHello : function() {
 				MessageToast.show("helloka");
