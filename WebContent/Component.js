209f2e76a060b925bd38f81f0226bb3440f44d45
kisbucedli <kisbucedli@gmail.com>
2018-12-01 13:42:48 +0100

    application runs

--- WebContent/Component.js
+++ WebContent/Component.js
@@ -9,20 +9,13 @@ sap.ui.define([
 			manifest: "json"
 		},
 		
-		createContent : function() {
-			return new sap.ui.view({
-				id : "Main",
-				viewName : "com.work.attendance.view.Main",
-				type : sap.ui.core.mvc.ViewType.XML
-			});
-		}
-//		init: function () {
-//            // call the init function of the parent
-//            UIComponent.prototype.init.apply(this, arguments);
-//
-//            // create the views based on the url/hash
-//            this.getRouter().initialize();
-//        }
+		init: function () {
+            // call the init function of the parent
+            UIComponent.prototype.init.apply(this, arguments);
+
+            // create the views based on the url/hash
+            //this.getRouter().initialize();
+        }
 
 	});
 
