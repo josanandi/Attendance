929aabcc5af1d1f8f329cc22cc27a8e54f043f68
kisbucedli <kisbucedli@gmail.com>
2018-12-01 13:06:45 +0100

    Component.js update index.html with bootstrap controller and view name with M

---
+++ WebContent/controller/Main.controller.js
@@ -0,0 +1,14 @@
+/*global location history */
+sap.ui.define([
+		"sap/ui/core/mvc/Controller"
+	], function (Controller) {
+		"use strict";
+
+		return BaseController.extend("com.work.attendance.controller.Main", {
+			
+			onInit : function () {
+
+			}
+		});
+    }
+);
