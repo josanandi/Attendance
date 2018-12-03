bc70ba95025798ac8971668ef18d673487b8e7bf
kisbucedli <kisbucedli@gmail.com>
2018-12-03 09:20:10 +0100

    Tabel view added and set the data with formatter.

--- WebContent/controller/Main.controller.js
+++ WebContent/controller/Main.controller.js
@@ -1,16 +1,20 @@
 /*global location history */
 sap.ui.define([
+		"com/work/attendance/model/formatter",
 		"sap/ui/core/mvc/Controller",
 		"sap/m/MessageToast"
-	], function (Controller,MessageToast) {
+	], function (formatter,Controller,MessageToast) {
 		"use strict";
 
 		return Controller.extend("com.work.attendance.controller.Main", {
 			
+			formatter: formatter,
 			onInit : function () {   
 	            var oModel= new sap.ui.model.json.JSONModel();
 	            oModel.loadData("./Attendance.json");
 			    this.getView().setModel(oModel,"attendances");
+			    
+			    
 			},
 			showHello : function() {
 				MessageToast.show("helloka");
---
+++ WebContent/model/formatter.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+	] , function () {
+		"use strict";
+
+		return {
+			
+			workDuration: function(sStart, sEnd) {
+				var iResult;
+				var myStartDate = new Date(sStart);
+				var myEndDate = new Date(sEnd);
+				iResult=(myEndDate.getHours())-(myStartDate.getHours());
+				return iResult;
+			}
+		};
+	}
+);
