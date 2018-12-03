6a98c169965fa8b65e832ed160768cbac8840967
kisbucedli <kisbucedli@gmail.com>
2018-12-03 15:58:34 +0100

    proxy set

--- WebContent/controller/Main.controller.js
+++ WebContent/controller/Main.controller.js
@@ -1,23 +1,33 @@
 /*global location history */
 sap.ui.define([
 		"com/work/attendance/model/formatter",
+		"sap/ui/model/odata/ODataModel",
 		"sap/ui/core/mvc/Controller",
 		"sap/m/MessageToast"
-	], function (formatter,Controller,MessageToast) {
+	], function (formatter,ODataModel,Controller,MessageToast) {
 		"use strict";
 
 		return Controller.extend("com.work.attendance.controller.Main", {
 			
 			formatter: formatter,
 			onInit : function () {   
-	            var oModel= new sap.ui.model.json.JSONModel();
-	            oModel.loadData("./Attendance.json");
-			    this.getView().setModel(oModel,"attendances");
-			    
-				var oViewModel = new sap.ui.model.json.JSONModel({
-					date : "2018-12-03"
-				});
-			    this.getView().setModel(oViewModel,"attendanceModel");
+//	            var oModel= new sap.ui.model.json.JSONModel();
+//	            oModel.loadData("./Attendance.json");
+//			    this.getView().setModel(oModel,"attendances");
+//			    
+//				var oViewModel = new sap.ui.model.json.JSONModel({
+//					date : "2018-12-03"
+//				});
+//			    this.getView().setModel(oViewModel,"attendanceModel");
+				var oOdataModel= new ODataModel("proxy/http/madap8033.accenture.tsap:8001/sap/opu/odata/sap/ZATTENDANCE_SRV"); 
+				oOdataModel.read("/ATTENDANCESet",
+						{
+						success: function(oData, oResponse) {
+							console.log(oData);
+						}
+						});
+				this.getView().setModel(oOdataModel,"attendances");
+
 
 			},
 			onUpdateFinished: function() {
@@ -27,7 +37,7 @@ sap.ui.define([
 				for(row = 0; row < oTable.getItems().length; row++)
 				{
 				        cell = oTable.getItems()[row].getCells()[4].getText(); // Would contain all cells in the table
-				        if(cell == 8 ){ 
+				        if(cell > 8 ){ 
 				        	oTable.getItems()[row].addStyleClass("overtime");
 				        } else {
 				        	oTable.getItems()[row].addStyleClass("correcttime");
