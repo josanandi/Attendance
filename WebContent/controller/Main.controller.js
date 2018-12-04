edae3a460e538e063cc713651493103f31800ff9
kisbucedli <kisbucedli@gmail.com>
2018-12-04 10:58:24 +0100

    The colorizing if the line works. The database is expanded. The filtering work for searching and started to implement the sorting.

--- WebContent/controller/Main.controller.js
+++ WebContent/controller/Main.controller.js
@@ -3,31 +3,34 @@ sap.ui.define([
 		"com/work/attendance/model/formatter",
 		"sap/ui/model/odata/ODataModel",
 		"sap/ui/core/mvc/Controller",
-		"sap/m/MessageToast"
-	], function (formatter,ODataModel,Controller,MessageToast) {
+		"sap/m/MessageToast",
+		"sap/ui/model/Filter",
+		"sap/ui/model/FilterOperator",
+		"sap/ui/model/FilterType"
+	], function (formatter,ODataModel,Controller,MessageToast, Filter,FilterOperator,FilterType) {
 		"use strict";
 
 		return Controller.extend("com.work.attendance.controller.Main", {
 			
 			formatter: formatter,
 			onInit : function () {   
-//	            var oModel= new sap.ui.model.json.JSONModel();
-//	            oModel.loadData("./Attendance.json");
-//			    this.getView().setModel(oModel,"attendances");
-//			    
-//				var oViewModel = new sap.ui.model.json.JSONModel({
-//					date : "2018-12-03"
-//				});
-//			    this.getView().setModel(oViewModel,"attendanceModel");
-				var oOdataModel= new ODataModel("proxy/http/madap8033.accenture.tsap:8001/sap/opu/odata/sap/ZATTENDANCE_SRV"); 
-				oOdataModel.read("/ATTENDANCESet",
-						{
-						success: function(oData, oResponse) {
-							console.log(oData);
-						}
-						});
-				this.getView().setModel(oOdataModel,"attendances");
-
+	            var oModel= new sap.ui.model.json.JSONModel();
+	            oModel.loadData("./Attendance.json");
+			    this.getView().setModel(oModel,"attendances");
+			    
+				var oViewModel = new sap.ui.model.json.JSONModel({
+					date : "2018-12-03"
+				});
+			    this.getView().setModel(oViewModel,"attendanceModel");
+			    
+//				var oOdataModel= new ODataModel("proxy/http/madap8033.accenture.tsap:8001/sap/opu/odata/sap/ZATTENDANCE_SRV"); 
+//				oOdataModel.read("/ATTENDANCESet",
+//						{
+//						success: function(oData, oResponse) {
+//							console.log(oData);
+//						}
+//						});
+//				this.getView().setModel(oOdataModel,"attendances");
 
 			},
 			onUpdateFinished: function() {
@@ -39,13 +42,21 @@ sap.ui.define([
 				        cell = oTable.getItems()[row].getCells()[4].getText(); // Would contain all cells in the table
 				        if(cell > 8 ){ 
 				        	oTable.getItems()[row].addStyleClass("overtime");
-				        } else {
-				        	oTable.getItems()[row].addStyleClass("correcttime");
-				        }
+				        } 
 				}
 			},
 			showHello : function() {
 				MessageToast.show("helloka");
+				
+			},
+			onFilterInvoices : function(){
+				var oView = this.getView(),
+					sValue = oView.byId("searchField").getValue(),
+					oFilter = new Filter("dailyTask/title",FilterOperator.Contains,sValue);
+				oView.byId("attendanceTable").getBinding("items").filter(oFilter,FilterType.Appliacation);
+				MessageToast.show("Filtered for: " + sValue);
+			},
+			onPressSorting: function() {
 				
 			}
 		});
